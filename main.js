fb4968bef2d786f3d738323b49780e7ef149dbfd
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-10-31 15:58:24 +1300

    built from cli test

---
+++ main.js
@@ -0,0 +1,12 @@
+// const sketch02 = require('./sketch-02.js')
+// const sketch03 = require('./sketch-03.js')
+
+// let index = 0
+
+
+// function next() {
+//   console.log('next')
+//   index++
+// }
+
+// sketchArray[index]
