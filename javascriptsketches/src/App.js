3a9def7b9cf0cd9d7c3f6d248c98e19b954af89f
Jeremy Hunter <jeremy.w.hunter@gmail.com>
2022-11-04 21:40:00 +1300

    added warning on load

--- javascriptsketches/src/App.js
+++ javascriptsketches/src/App.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react'
 import Container from './Container'
+import Warning from './Warning'
 import './App.css'
 
 function App() {
   const [bool, setBool] = useState(true)
   const [index, setIndex] = useState(false)
-
+  const [warning, setWarning] = useState(true)
   return (
     <>
       <div className="app">
-        <Container index={index} />
-        <button
-          onClick={() => {
-            setIndex(!index)
-            console.log('click', index)
-          }}
-        >
-          Change
-        </button>
+        {warning ? (
+          <div>
+            <Warning />{' '}
+            <button
+              onClick={() => {
+                setWarning(false)
+              }}
+            >
+              Continue?
+            </button>
+          </div>
+        ) : (
+          <div>
+            {' '}
+            <Container index={index} />
+            <button
+              onClick={() => {
+                setIndex(!index)
+                console.log('click', index)
+              }}
+            >
+              Change
+            </button>{' '}
+          </div>
+        )}
       </div>
     </>
   )
---
+++ javascriptsketches/src/Warning.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function warning() {
+  return (
+    <>
+      <h1>Maybe be harmful to photosensitive people</h1>
+      
+    </>
+  )
+}
